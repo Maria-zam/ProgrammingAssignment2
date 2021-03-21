791f752930bfaf27e05a89807d4d4a8a28015074
Maria-zam <81103142+Maria-zam@users.noreply.github.com>
2021-03-21 22:10:41 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The functions that I introduce there are supposed to make an object that keeps the matrix and then cashes the inverse of it. makeCacheMatrix is here to create matrix that is a list that contains a function. 
+## this function set and get the value of matrix and the inverse of it. Firstly it sets the value of the matrix, then it gets its value, sets the velue of inverse and gets the value of it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ variable <- NULL
+  set <- function(n) {
+       x <<- n
+       variable <<- NULL
+  }
+  get <- function() x
+  sinverse <- function(inverse) variable <<- inverse
+  ginverse <- function() variable
+  list( set =  set ,
+       get = get,
+        sinverse =  sinverse,
+       ginverse = ginverse)
 }
 
-
-## Write a short comment describing this function
+## Then there is a function that give us the inverse og the matrix, that was made by previous function. Supposing that matrix did not change and inverse has been already computed, thtis function give us the extraction of what has been cashed.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        r <- x$getinverse()
+   if (!is.null(r)) {
+          message("getting cached data")
+          return(r)
+   }
+   data <- x$get()
+   r <- solve(data, ...)
+   x$setinverse(r)
+   r
 }
+
+
